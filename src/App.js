30ce9a91f1c5e9eb41b20166a855c6284b425411
nick l <nlevy1@ufl.edu>
2025-05-03 23:29:15 -0400

    Skeleton

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import 'bootstrap/dist/js/bootstrap.js';
+import 'bootswatch/dist/darkly/bootstrap.min.css'
+
+function App() {
+  return (
+    <div>
+      hello
+    </div>
+      
+  );
+}
+
+export default App;
